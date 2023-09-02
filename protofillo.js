29ab2cefd3d318b83a0e2f3492e4ee59b830a90e
abdelrahmanmohamedr <139080958+abdelrahmanmohamedr@users.noreply.github.com>
2023-09-02 16:33:24 +0300

    Add files via upload

--- protofillo.js
+++ protofillo.js
@@ -8,7 +8,7 @@ let wn = n.clientWidth;
 let hn = n.clientHeight;
 
 
-if (b >= 1050){
+if (b >= 1600){
 function right(ee , w , h , wn , hn) { ee.style.transform =  `translate(${w-hn}px) rotate(90deg)`
 ee.style.fontSize = "17px"};
 function down(ee , w , h , wn , hn){ee.style.transform = `translate(${w}px, ${h-hn-15}px) rotate(180deg)`
@@ -65,104 +65,104 @@ up.style.display = 'none'
 
 console.log(i1)
 
-console.log(b)
+window.onscroll = () => {
+    console.log(window.scrollY);
+    console.log(b);
 
-window.onscroll = function(){
-    console.log(window.scrollY)
-    console.log(b)
-    if (b >= 1600){ 
-        if(window.scrollY >= 3038.1064453125){
-         side.style.cssText = `position : absolute;
+    if (b >= 1600) {
+        if (window.scrollY >= 3038.1064453125) {
+            side.style.cssText = `position : absolute;
          bottom:0;
-         height:969px`
-        }else{
-         side.style.cssText = `position : fixed;`
-        }}
-        if (b >= 1600){ 
-        if(window.scrollY < 400){
-        up.style = `display : none`;
-        }else {
-        up.setAttribute("style" , `
+         height:969px`;
+        } else {
+            side.style.cssText = `position : fixed;`;
+        }
+    }
+    if (b >= 1600) {
+        if (window.scrollY < 400) {
+            up.style = `display : none`;
+        } else {
+            up.setAttribute("style", `
         display : inline;
         transition-duration: 3s; 
-        ` )
+        `);
         }
-        }
-        else{
-        if(window.scrollY < 50){
-        up.style = `display : none`;
-        }else {
-        up.setAttribute("style" , `
+    }
+    else {
+        if (window.scrollY < 50) {
+            up.style = `display : none`;
+        } else {
+            up.setAttribute("style", `
         display : inline;
         transition-duration: 3s; 
-        ` )
+        `);
         }
+    }
+    if (b >= 1600) {
+        if (window.scrollY >= 135.35000610351562) {
+            a.style.display = "block";
+            a.style.opacity = "1";
+            a.style.transform = "translate(0px)";
+            i.style.display = "block";
         }
-        if (b >= 1600){
-        if(window.scrollY >= 135.35000610351562){
-        a.style.display = "block"
-        a.style.opacity = "1";
-        a.style.transform = "translate(0px)"
-        i.style.display = "block"
-        }
-        if(window.scrollY >= 615.5){
-        i.style.opacity = "1";
-        i.style.transform = "translate(0px)"
-        sk.style.display = "block"
-        }
-        if(window.scrollY >= 1200.39990234375){
-        sk.style.opacity = "1";
-        sk.style.transform = "translate(0px)"
-        p.style.display = "grid"
-        }
-        if(window.scrollY >= 2370){
-        p.style.opacity = "1";
-        i1.style.animation = "pop 1.5s"
-        i2.style.animation = "pop 1.5s"
-        i3.style.animation = "pop 1.5s"
-        ce.style.display = "grid"
-        }
-        if(window.scrollY >= 2648.541015625){
-        ce.style.opacity = "1";
-        i_.style.animation = "pop 1.5s"
-        ii.style.animation = "pop 1.5s"
-        iii.style.animation = "pop 1.5s"
-        vi.style.animation = "pop 1.5s"
-        v.style.animation = "pop 1.5s"
-        iv.style.animation = "pop 1.5s"
-        f.style.opacity = "1"
-        }
-    }else{
-        if(window.scrollY >= 0){
-        a.style.display = "block"
-        a.style.opacity = "1";
-        a.style.transform = "translate(0px)"
-        i.style.display = "block"
-        }
-        if(window.scrollY >= 615.5){
+        if (window.scrollY >= 615.5) {
             i.style.opacity = "1";
-            i.style.transform = "translate(0px)"
-            sk.style.display = "block"
-            }
-            if(window.scrollY >= 670){
+            i.style.transform = "translate(0px)";
+            sk.style.display = "block";
+        }
+        if (window.scrollY >= 1200.39990234375) {
             sk.style.opacity = "1";
-            sk.style.transform = "translate(0px)"
-            p.style.display = "grid"
-            }
-            if(window.scrollY >= 2300){
+            sk.style.transform = "translate(0px)";
+            p.style.display = "grid";
+        }
+        if (window.scrollY >= 2370) {
             p.style.opacity = "1";
-            ce.style.display = "grid"
-            }
-            if(window.scrollY >= 2648.541015625){
+            i1.style.animation = "pop 1.5s";
+            i2.style.animation = "pop 1.5s";
+            i3.style.animation = "pop 1.5s";
+            ce.style.display = "grid";
+        }
+        if (window.scrollY >= 2648.541015625) {
             ce.style.opacity = "1";
-            i_.style.animation = "pop 1.5s"
-            ii.style.animation = "pop 1.5s"
-            iii.style.animation = "pop 1.5s"
-            vi.style.animation = "pop 1.5s"
-            v.style.animation = "pop 1.5s"
-            iv.style.animation = "pop 1.5s"
-            f.style.opacity = "1"
-            }
+            i_.style.animation = "pop 1.5s";
+            ii.style.animation = "pop 1.5s";
+            iii.style.animation = "pop 1.5s";
+            vi.style.animation = "pop 1.5s";
+            v.style.animation = "pop 1.5s";
+            iv.style.animation = "pop 1.5s";
+            f.style.opacity = "1";
+        }
+    } else {
+        if (window.scrollY >= 0) {
+            a.style.display = "block";
+            a.style.opacity = "1";
+            a.style.transform = "translate(0px)";
+            i.style.display = "block";
+        }
+        if (window.scrollY >= 615.5) {
+            i.style.opacity = "1";
+            i.style.transform = "translate(0px)";
+            sk.style.display = "block";
+        }
+        if (window.scrollY >= 670) {
+            sk.style.opacity = "1";
+            sk.style.transform = "translate(0px)";
+            p.style.display = "grid";
+        }
+        if (window.scrollY >= 2300) {
+            p.style.opacity = "1";
+            ce.style.display = "grid";
+        }
+        if (window.scrollY >= 2648.541015625) {
+            ce.style.opacity = "1";
+            i_.style.animation = "pop 1.5s";
+            ii.style.animation = "pop 1.5s";
+            iii.style.animation = "pop 1.5s";
+            vi.style.animation = "pop 1.5s";
+            v.style.animation = "pop 1.5s";
+            iv.style.animation = "pop 1.5s";
+            f.style.opacity = "1";
+        }
     }
 }
 //_______________________________________________________________________________________________________________________________________________________________________________
